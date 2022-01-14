9829c3f84f365d7c43ec083b0c222a2c99a0c941
agistarakha <agistarakhasnc@gmail.com>
2022-01-14 17:29:58 +0700

    0.2.5.2
    - Windowed optin
    - Door di room E

--- Assets/Scripts/Option/Video/WindowedModeConf.cs
+++ Assets/Scripts/Option/Video/WindowedModeConf.cs
@@ -2,16 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class WindowedModeConf : MonoBehaviour
 {
+    TMP_Text[] texts;
     // Start is called before the first frame update
     void OnEnable()
     {
+        texts = GetComponentsInChildren<TMP_Text>();
+
         Button[] buttons = transform.GetComponentsInChildren<Button>();
         buttons[0].onClick.AddListener(() => Screen.fullScreenMode = FullScreenMode.Windowed);
         buttons[1].onClick.AddListener(() => Screen.fullScreenMode = FullScreenMode.FullScreenWindow);
+        buttons[0].onClick.AddListener(() => SetTextAlpha(0));
+        buttons[1].onClick.AddListener(() => SetTextAlpha(1));
+        FullScreenCheck();
 
+
+
+    }
+
+    void OnDisable()
+    {
+        Button[] buttons = transform.GetComponentsInChildren<Button>();
+        buttons[0].onClick.RemoveAllListeners();
+        buttons[1].onClick.RemoveAllListeners();
+    }
+
+
+    private void SetTextAlpha(int mode)
+    {
+        if (mode == 0)
+        {
+            texts[0].alpha = 1f;
+            texts[1].alpha = 0.5f;
+        }
+        else if (mode == 1)
+        {
+            texts[1].alpha = 1f;
+            texts[0].alpha = 0.5f;
+        }
+    }
+    private void FullScreenCheck()
+    {
+        if (Screen.fullScreenMode == FullScreenMode.Windowed)
+        {
+            texts[0].alpha = 1f;
+            texts[1].alpha = 0.5f;
+        }
+        else if (Screen.fullScreenMode == FullScreenMode.FullScreenWindow)
+        {
+            texts[1].alpha = 1f;
+            texts[0].alpha = 0.5f;
+        }
     }
 
 
